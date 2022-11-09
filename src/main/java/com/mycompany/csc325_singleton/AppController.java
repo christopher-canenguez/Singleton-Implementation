b243633801ff33809b00b9ce124c5e96725f2486
Christopher Canenguez <canenguez.chris@gmail.com>
2022-11-09 14:05:19 -0500

    First Commit.

--- src/main/java/com/mycompany/csc325_singleton/AppController.java
+++ src/main/java/com/mycompany/csc325_singleton/AppController.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.csc325_singleton;
 
 /**
@@ -6,23 +5,24 @@ package com.mycompany.csc325_singleton;
  * @author MoaathAlrajab
  */
 public class AppController {
-    
+
     private static AppController instance;
-    
-    private AppController(){
+
+    private AppController() {
         System.out.println("Instance created");
-    }
-    
-    public static AppController getInstance(){
-        if( instance == null){
-            
-            synchronized(AppController.class){
-                if( instance == null)
+    } // End AppController.
+
+    public static AppController getInstance() {
+        if (instance == null) {
+
+            synchronized (AppController.class) {
+                if (instance == null) {
                     instance = new AppController();
-            }
-     
-        }
+                } // End if.
+            } // End synchronized.
+
+        } // End if.
         return instance;
-    }
-    
-}
+    } // End AppController.
+
+} // End AppController.
--- src/main/java/com/mycompany/csc325_singleton/DriverClass.java
+++ src/main/java/com/mycompany/csc325_singleton/DriverClass.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.csc325_singleton;
 
 /**
@@ -6,30 +5,24 @@ package com.mycompany.csc325_singleton;
  * @author MoaathAlrajab
  */
 public class DriverClass {
-    
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                AppController app1= AppController.getInstance();
-            }
-            
-        });
-        
-        
-          Thread thread2 = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                AppController app2= AppController.getInstance();
-            }
-            
-        });
 
-         thread1.start();
-         //Thread.sleep(1);
-         thread2.start();
-          
-   
-    }
-    
-}
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AppController app1 = AppController.getInstance();
+            } // End run.
+        }); // End Thread.
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AppController app2 = AppController.getInstance();
+            } // End run.
+        }); // End Thread.
+
+        thread1.start();
+        //Thread.sleep(1);
+        thread2.start();
+    } // End main.
+} // End DriverClass.
